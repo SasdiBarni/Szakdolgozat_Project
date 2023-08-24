38c5f36c57ce4391717590dfa2667920dcd9bddc
SasdiBarni <sasdibarni@stud.uni-obuda.hu>
2023-08-24 09:38:47 +0200

    Tried different cell detection methods

---
+++ CENTRAL_SERVER/CellSeedDetection.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+from numpy.lib.polynomial import poly
+
+img = cv2.imread('C:\\Users\\sasdi\\Documents\\Szakdolgozat_Other\\Sample_1.bmp')
+    
+blur = cv2.medianBlur(img, 5)
+gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+
+thresh_for_black_dots = cv2.threshold(gray,100,255, cv2.THRESH_BINARY_INV)[1]
+
+cnts_for_black_dots = cv2.findContours(thresh_for_black_dots, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+cnts_for_black_dots = cnts_for_black_dots[0] if len(cnts_for_black_dots) == 2 else cnts_for_black_dots[1]
+
+min_area = 1
+black_dots = []
+        
+for c in cnts_for_black_dots:
+   area = cv2.contourArea(c)
+   if area > min_area:
+      cv2.drawContours(img, [c], -1, (36, 255, 12), 1)
+      black_dots.append(c)
+
+print("Cell Seed Count is:",len(black_dots))
+
+cv2.imshow('Output image:', img)
+cv2.waitKey()
+
+'''
+im = cv2.imread('C:\\Users\\sasdi\\Documents\\Szakdolgozat_Other\\Sample_4.bmp')
+gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+# Set up the detector with default parameters.
+detector = cv2.SimpleBlobDetector_create()
+ 
+# Detect blobs.
+keypoints = detector.detect(gray)
+print("Cellseed Count is:",len(keypoints))
+
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+ 
+# Show keypoints
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
+'''
+
+'''
+img = cv2.imread('C:\\Users\\sasdi\\Documents\\Szakdolgozat_Other\\Sample_4.bmp')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+
+
+#cv2.imshow('Binary image', thresh)
+#cv2.imshow('image_thres1.jpg', thresh)
+#cv2.waitKey(0)
+
+contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+
+image_copy = img.copy()
+cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=1, lineType=cv2.LINE_AA)
+                
+# see the results
+cv2.imshow('None approximation', image_copy)
+cv2.imshow('contours_none_image1.jpg', image_copy)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+print(len(contours))
+
+
+# filter by area
+s1 = 3
+s2 = 20
+xcnts = []
+for cnt in contours:
+    if s1<cv2.contourArea(cnt) <s2:
+        xcnts.append(cnt)
+        
+print("\nDots number: {}".format(len(xcnts)))
+'''
--- CENTRAL_SERVER/SlideApplication.py
+++ CENTRAL_SERVER/SlideApplication.py
@@ -1,13 +1,15 @@
 import simpleslideinterface
 import json
 import numpy as np
+from numpy import asarray
 
 base_url='http://localhost:5120/'
 ssi = simpleslideinterface.MinimalWrapper(base_url=base_url, raise_for_status=True)
 
-#! ADD TO FRONT OF PATH '\\\\192.168.0.1\\' SERVER IP
+
 def OpenSlide(ssi, directoryName):
     
+    #! ADD TO FRONT OF PATH '\\\\192.168.0.1\\' SERVER IP AND CHANGE ROUTE
     slide_path = f'C:\\Users\\sasdi\\Documents\\Szakdolgozat_Project\\FILE_SERVER\\slides\\{directoryName}\\{directoryName}'
     slide_token = ssi.post('slide/open/local/{}', slide_path, readonly=True).json()
     print('Created slide token:', slide_token)
@@ -22,16 +24,18 @@ def GetTilesFromSlide(ssi, slide_token, properties):
     
     width = int(int(properties['Width']) / 256 / 2)
     height = int(int(properties['Height']) / 256 / 2)
-    
-    #! Saving is not needed for tiles    
+      
     for i  in range(width):
         for j in range(height):
-            image = ssi.get_image('slide/{}/tile', slide_token, encoding='BMP_RAW', x1=i, y1=j, x2=i, y2=j, magnification=1)
             
-            if np.mean(image) == 255:
-                print()
+            #converting img to npArray to filter out all white imgs
+            image = ssi.get_image('slide/{}/tile', slide_token, encoding='BMP_RAW', x1=i, y1=j, x2=i, y2=j, magnification=1)
+            numpyArray= asarray(image)            
+            
+            if np.mean(numpyArray) == 255:
+                continue
             else:
-                print()
-                #else ágon lesz a feldolgozás
+                #!CELL SEED DETECTION, PUT HERE
+                continue
                 
             #image.save('C:\\Users\\sasdi\\Documents\\Szakdolgozat_Project\\CENTRAL_SERVER\\TILES\\' + slide_token + '__'+ str(i) + '__' + str(j) + '.bmp')
