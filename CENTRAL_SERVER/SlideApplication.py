a8aec698cfa702f799d563c584e4effb24bdfc61
SasdiBarni <sasdibarni@stud.uni-obuda.hu>
2023-10-10 15:13:17 +0200

    Removed the filter from segmentation

--- CENTRAL_SERVER/SlideApplication.py
+++ CENTRAL_SERVER/SlideApplication.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 from numpy import asarray
 import cv2
-import os
+import os, time
 
 def OpenSlide(directoryName, jobId, date, user):
     
@@ -29,6 +29,8 @@ def GetTilesFromSlide(ssi, slide_token, properties, jobId, directoryName, date, 
     
     if jobId == 'Cell seed detection and counting':
         
+        start_time = time.time()
+        
         #seedNum = 0
         seedNums = []
         
@@ -41,6 +43,27 @@ def GetTilesFromSlide(ssi, slide_token, properties, jobId, directoryName, date, 
                 image = ssi.get_image('slide/{}/tile', slide_token, encoding='BMP_RAW', x1=i, y1=j, x2=i, y2=j, magnification=1)
                 numpyArray= asarray(image)
                 
+                print(f'---PROCESSING--- {i + 1}/{width} - {j + 1}/{height} ', end = '\r')
+                
+                gray = cv2.cvtColor(numpyArray, cv2.COLOR_BGR2GRAY)
+
+                thresh_for_seeds = cv2.threshold(gray, 135, 255, cv2.THRESH_TOZERO_INV)[1]
+
+                cnts_for_seeds = cv2.findContours(thresh_for_seeds, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                cnts_for_seeds = cnts_for_seeds[0] if len(cnts_for_seeds) == 2 else cnts_for_seeds[1]
+
+                min_area = 1
+                black_dots = []
+
+                for c in cnts_for_seeds:
+                    area = cv2.contourArea(c)
+                    if area > min_area:
+                        cv2.drawContours(numpyArray, [c], -1, (36, 255, 12), 1)
+                        black_dots.append(c)
+                
+                seedNums.append(len(black_dots))
+                '''
                 if np.mean(numpyArray) == 255:
                     print(f'---PROCESSING--- {i + 1}/{width} - {j + 1}/{height} ', end = '\r')
                     continue
@@ -66,21 +89,20 @@ def GetTilesFromSlide(ssi, slide_token, properties, jobId, directoryName, date, 
                     
                     #seedNum += len(black_dots)
                     seedNums.append(len(black_dots))
+                '''
         
-        path = f'C:\\Users\\sasdi\\Documents\\Szakdolgozat_Project\\FILE_SERVER\\results\\{str(user)}_{str(directoryName)}_{str(jobId)}_results.txt'
+        end_time = time.time()
         
-        check_file = os.path.exists(path)
+        process_time = start_time - end_time
         
-        if not check_file:
-            result = open('C:\\Users\\sasdi\\Documents\\Szakdolgozat_Project\\FILE_SERVER\\results\\' + str(user) + '_' + str(directoryName) + '_' + str(jobId) + '_results.txt', 'w')
-            for num in seedNums:
-                result.write(f'{num}\n')
-            #result.write(f'{date} - {user} - {directoryName} - {jobId} :: {str(seedNum)}\n')
-            print('[SERVER] Finished!')
-        else:
-            for num in seedNums:
-                result.write(f'{num}\n')
-            #result.write(f'{date} - {user} - {directoryName} - {jobId} :: {str(seedNum)}\n')
-            print('[SERVER] Finished!')
+        print('Process Time: ' + str(process_time))
+        
+        result = open('C:\\Users\\sasdi\\Documents\\Szakdolgozat_Project\\FILE_SERVER\\results\\' + str(user) + '_' + str(directoryName) + '_' + str(jobId) + '_results.txt', 'w')
+        
+        for num in seedNums:
+            result.write(f'{num}\n')
+            
+        #result.write(f'{date} - {user} - {directoryName} - {jobId} :: {str(seedNum)}\n')
+        print('[SERVER] Finished!')
         
         #result = open(f'media\\nfs\\results\\{directoryName}.txt', 'w')
